a7222d9f9c72529c10a8a0613f4659b366d357d3
lixelamos <amosbett99@gmail.com>
2023-05-25 01:38:21 +0300

    completed assignment

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        addresses = self.email_addresses.replace(",", " ").split()
+        unique_addresses = list(set(addresses))
+        sorted_addresses = sorted(unique_addresses)
+        email_addresses = [address for address in sorted_addresses if self.is_email(address)]
+        return email_addresses
+
+    def is_email(self, string):
+        # Use a simple regex pattern to check if the string is a valid email address
+        import re
+        pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+        return re.match(pattern, string) is not None
